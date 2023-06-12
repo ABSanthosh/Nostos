fef8d87e1da5d7e556a5ef7e2732690f74aebc15
ABSanthosh <a.b.santhosh02@gmail.com>
2023-06-12 15:55:26 +0530

    feat: basic setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import Head from "next/head";
+import "../styles/root/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <title>Nostos</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Nostos is a web application that allows users to create and share their own interactive stories."
+        />
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
